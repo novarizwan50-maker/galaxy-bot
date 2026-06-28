4fd564eb642c594144a1f706c7ed3066296d6b18
novarizwan50-maker <novarizwan50@gmail.com>
2026-06-28 16:36:25 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("🌌 Galaxy Bot Started!");
+console.log("👑 Owner: Rushan Khan");
+console.log("🛡️ Head Admin: Arman");
+
+setInterval(() => {
+  console.log("💜 Galaxy Bot is running...");
+}, 60000);
